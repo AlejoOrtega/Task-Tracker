45737acb39d439068fb6267a3cd14b29f1cc4e9e
Alejandro Ortega <ajrevollo@uninorte.edu.co>
2021-09-18 23:09:05 -0400

    Task and AddTask

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import PropTypes from 'prop-types'
+import Button from "./Button"
 
 const Header = ({title}) => {
+
+    const onClick = ()=>{
+        console.log("click")
+    }
+
     return (
         <header class='header'>
            <h1>{title}</h1>
-           <button className='btn'>Add</button>
+           <Button color='green' onClick={onClick}/>
+           
         </header>
     )
 }
